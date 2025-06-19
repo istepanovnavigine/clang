73fd67914c835181288d12f66bfd23c2a1a9d3b2
navigine <navigine@debian.debian>
2025-06-19 16:13:23 +0300

    added inputstr

--- src/main.c
+++ src/main.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <limits.h>
 
+#define MAXLEN 20
+
+int inputline(char s[MAXLEN]);
+
 int main() {
-    printf("char range: %d\n", (CHAR_MAX - CHAR_MIN));
-    printf("short range: %d\n", (SHRT_MAX - SHRT_MIN));
-    printf("int range: %u\n", (unsigned int)(INT_MAX) - INT_MIN);
-    printf("long range: %lu\n", (unsigned long)(LONG_MAX) - LONG_MIN);
-    printf("unsigned char range: %d\n", UCHAR_MAX);
-    printf("unsigned int range: %u\n", (unsigned int)(UINT_MAX));
-    printf("unsigned long range: %lu\n", (unsigned long)(ULONG_MAX));
-    printf("unsigned short range: %u\n", USHRT_MAX);
-    printf("lol\n");
+    char hex[MAXLEN];
+
+    inputline(hex);
+
+    printf("%s", hex);
 
     return 0;
+}
+
+int htoi(char s[MAXLEN]) {
+    int result = 0;
+
+
+
+    return result;
+}
+
+int inputline(char s[MAXLEN]) {
+    int i, c;
+
+    for(i = 0; i < MAXLEN - 1; i++) {
+        c = getchar();
+        if(c == '\n' || c == '\0') break;
+        s[i] = c;
+    }
+    s[i++] = '\n';
+    s[i++] = '\0';
+
+    return i;
 }
