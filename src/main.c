94c063ca2533eeffda9cfeef6257d8e1e975f073
navigine <navigine@debian.debian>
2025-06-27 15:31:01 +0300

    not working now

--- src/main.c
+++ src/main.c
@@ -13,6 +13,8 @@ int main() {
     inputline(s1);
     inputline(s2);
 
+    printf("\n%s\n", s1);
+
     return 0;
 }
 
@@ -32,5 +34,28 @@ int inputline(char s[MAXLEN]) {
 }
 
 int squeeze(char s1[MAXLEN], char s2[MAXLEN]) {
-    
+    int i = 0;
+    int j = 0;
+    char buf[MAXLEN];
+
+    while ((s1[j] != '\n' && s1[j] != '\0') && (s2[j] != '\n' && s2[j] != '\0')) {
+        if (s1[j] != s2[j]) {
+            buf[i++] = s1[j];   
+        }
+        j++;
+    }
+
+    while (s1[j] != '\n' && s1[j] != '\0') {
+        buf[i++] = s1[j++];
+    }
+
+    buf[i++] = s1[j++];
+
+    for(j = 0; j < i; j++) {
+        s1[j] = buf[j];
+    }
+
+    s1[++j] = '\0';
+
+    return i;
 }
