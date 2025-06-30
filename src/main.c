c83be15c828cb38f8282916f19626adfcf04a1f5
navigine <navigine@debian.debian>
2025-06-30 10:07:54 +0300

    done task24

--- src/main.c
+++ src/main.c
@@ -13,6 +13,8 @@ int main() {
     inputline(s1);
     inputline(s2);
 
+    squeeze(s1, s2);
+
     printf("\n%s\n", s1);
 
     return 0;
@@ -38,24 +40,27 @@ int squeeze(char s1[MAXLEN], char s2[MAXLEN]) {
     int j = 0;
     char buf[MAXLEN];
 
-    while ((s1[j] != '\n' && s1[j] != '\0') && (s2[j] != '\n' && s2[j] != '\0')) {
+    
+    while ((s1[j] != '\n' && s1[j] != '\0') && (s2[j] != '\n' && s2[j] != '\0')) { 
         if (s1[j] != s2[j]) {
             buf[i++] = s1[j];   
         }
         j++;
     }
 
+ 
     while (s1[j] != '\n' && s1[j] != '\0') {
         buf[i++] = s1[j++];
     }
 
-    buf[i++] = s1[j++];
+    
+
+    buf[i++] = '\n';
+    buf[i++] = '\0';
 
     for(j = 0; j < i; j++) {
         s1[j] = buf[j];
     }
-
-    s1[++j] = '\0';
 
     return i;
 }
