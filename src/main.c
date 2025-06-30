0fd96312ee08b1f401b4f1feb69ba3dc48b59be6
navigine <navigine@debian.debian>
2025-06-30 17:59:47 +0300

    started task26

--- src/main.c
+++ src/main.c
@@ -1,59 +1,15 @@
 #include <stdio.h>
 
-#define MAXLEN 20
-
-
-int inputline(char s[MAXLEN]);
-int any(char s1[MAXLEN], char s2[MAXLEN]);
+int setbits(int x, int p, int n, int y);
 
 int main() {
-    char s1[MAXLEN];
-    char s2[MAXLEN];
-    int pos;
+    int poison = 128 << 1;
 
-    inputline(s1);
-    inputline(s2);
-
-    pos = any(s1, s2);
-
-    if(pos == -1) {
-        printf("\nno considences\n");
-    } else {
-        printf("\nfirst index in s1 from s2 = %d\n", pos);
-    }
+    printf("%d\n", poison);
 
     return 0;
 }
 
+int setbits(int x, int p, int n, int y) {
 
-int inputline(char s[MAXLEN]) {
-    int i, c;
-
-    for(i = 0; i < MAXLEN - 1; i++) {
-        c = getchar();
-        if(c == '\n' || c == '\0') break;
-        s[i] = c;
-    }
-    s[i++] = '\n';
-    s[i++] = '\0';
-
-    return i;
-}
-
-int any(char s1[MAXLEN], char s2[MAXLEN]) {
-    int pos = -1;
-    
-    for (int i = 0; s2[i] != '\n'; i++) {
-        for (int j = 0; s1[j] != '\n'; j++) {
-            if(s1[j] == s2[i]) {
-                if(pos != -1 && j < pos) {
-                    pos = j;
-                } else {
-                    pos = j;
-                }
-            }
-        }
-    }
-
-    return pos; 
 }
