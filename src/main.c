073a461613469832794f1fcf643099c6c1fe1120
navigine <navigine@debian.debian>
2025-07-04 18:01:47 +0300

    xd

--- src/main.c
+++ src/main.c
@@ -3,13 +3,19 @@
 int setbits(int x, int p, int n, int y);
 
 int main() {
-    int poison = 128 << 1;
+    int x = 0, p = 0, n = 0, y = 0;
+    scanf("%d\n", x);
+    scanf("%d\n", p); 
+    scanf("%d\n", n);
+    scanf("%d\n", y);
 
-    printf("%d\n", poison);
+
+    printf("%d\n", x);
 
     return 0;
 }
 
 int setbits(int x, int p, int n, int y) {
-
+    
 }
+
