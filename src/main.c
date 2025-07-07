852ddf029a7052fd7b4d0fe37627447fc5c2ad6f
navigine <navigine@debian.debian>
2025-07-07 17:42:13 +0300

    done 27

--- src/main.c
+++ src/main.c
@@ -1,41 +1,27 @@
 #include <stdio.h>
 
-int setbits(int x, int p, int n, int y);
+int invert(int x, int p, int n);
 
 int main() {
-    int x = 0, p = 0, n = 0, y = 0;
+    int x = 0, p = 0, n = 0;
     printf("\nВведите число x: ");
     scanf("%d", &x);
     printf("\nВведите число p: ");
     scanf("%d", &p); 
     printf("\nВведите число n: ");
     scanf("%d", &n);
-    printf("\nВведите число y: ");
-    scanf("%d", &y);
 
-    x = setbits(x, p, n, y);
+    x = invert(x, p, n);
 
     printf("x после преобразования: %d\n", x);
 
     return 0;
 }
 
-int setbits(int x, int p, int n, int y) {
-    int num_for_and = 1;
-    for(int i = 0; i < x - n - p; i++) {
-        num_for_and = num_for_and << 1;
-        num_for_and++;
-    }
-    num_for_and = num_for_and << p;
-    
-    for(int i = 0; i < n; i++) {
-        num_for_and = num_for_and << 1;
-        num_for_and++;
-    }
+int invert(int x, int p, int n) {
+    int mask = ((1 << n) - 1) << p;
 
-    x = x & num_for_and; // Получен x после вычета
-
-    
+    x = x ^ mask;
 
     return x;
 }
